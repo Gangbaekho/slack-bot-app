d4c3e9b961684bbc50107713cc6a068252412463
kimjinsoo <roorooro9933@gmail.com>
2021-09-09 00:49:30 +0900

    fetch workspaces and users

--- src/component/Wrapper.js
+++ src/component/Wrapper.js
@@ -23,55 +23,7 @@ const Wapper = (props) => {
         rounded={'md'}
         p={6}
         overflow={'hidden'}>
-        <Stack>
-          <Text
-            color={'green.500'}
-            textTransform={'uppercase'}
-            fontWeight={800}
-            fontSize={'sm'}
-            letterSpacing={1.1}
-            py={3}>
-            SELECT WORK SPACE
-          </Text>
-          <Select placeholder="Workspace select">
-            <option value="workspace1">Workspace one</option>
-            <option value="workspace2">Workspace two</option>
-            <option value="workspace3">Workspace three</option>
-          </Select>
-          <Text
-            color={'green.500'}
-            textTransform={'uppercase'}
-            fontWeight={800}
-            fontSize={'sm'}
-            letterSpacing={1.1}
-            py={3}>
-            SELECT USER
-          </Text>
-          <Select>
-            <option value="all" selected>All of them</option>
-            <option value="user1">User one</option>
-            <option value="user2">User two</option>
-            <option value="user3">User three</option>
-          </Select>
-          <Heading
-            color={useColorModeValue('gray.700', 'white')}
-            fontSize={'2xl'}
-            fontFamily={'body'}
-            py={5}>
-            Send Message
-          </Heading>
-          <Input placeholder="Basic usage" />
-        </Stack>
-        <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
-          <Avatar
-            src={'https://avatars0.githubusercontent.com/u/1164541?v=4'}
-            alt={'Author'}
-          />
-          <Stack direction={'column'} spacing={0} fontSize={'sm'}>
-            <Text fontWeight={600}>I'm your ALOCADOS BOT!</Text>
-            <Text fontWeight={600}>Send message to workspaces!</Text>
-          </Stack>
-        </Stack>
+         {props.children}
       </Box>
     </Center>
   );
--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import { Box, Heading, Stack, Text } from '@chakra-ui/layout';
+import { Avatar, Button, Input, Select, useColorModeValue } from '@chakra-ui/react';
 import Wrapper from '../component/Wrapper'
 
+import {baseURL} from '../const'
+
+
 const HomePage = (props) =>{
+
+    const [workSpaces, setWorkSpaces] = useState([]);
+    const [workSpaceId, setWorkSpaceId] = useState(undefined);
+    const [users, setUsers] = useState([]);
+    const [userId, setUserId] = useState(undefined);
+    const [message, setMessage] = useState('');
+
+    useEffect(()=>{
+        fetch(baseURL+'/api/v1/workspaces',{
+            method:'GET',
+            'Content-Type':'application/json'
+        })
+        .then(res => res.json())
+        .then(data => {
+          setWorkSpaces(data.list)  
+        })
+    },[])
+
+    useEffect(()=>{
+        if(workSpaceId !== undefined){
+            fetch(baseURL+'/api/v1/users',{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json',
+                },
+                body:JSON.stringify({team_id:'T01ABNQ56F4'})  
+            })
+            .then(res => res.json())
+            .then(data => {
+              setUsers(data.list)  
+            })
+        }
+    },[workSpaceId])
+
     return (
-        <Wrapper/>
+        <Wrapper>
+               <Stack>
+          <Text
+            color={'green.500'}
+            textTransform={'uppercase'}
+            fontWeight={800}
+            fontSize={'sm'}
+            letterSpacing={1.1}
+            py={3}>
+            SELECT WORK SPACE
+          </Text>
+          <Select placeholder="Workspace select" onChange={(e)=>setWorkSpaceId(e.target.value)}>
+              {workSpaces.map(workspace => (<option key={workspace.id} value={workspace.id}>{workspace.name}</option>))}
+          </Select>
+          <Text
+            color={'green.500'}
+            textTransform={'uppercase'}
+            fontWeight={800}
+            fontSize={'sm'}
+            letterSpacing={1.1}
+            py={3}>
+            SELECT USER
+          </Text>
+          <Select onChange={(e)=>setUserId(e.target.value)}>
+            <option value="all" defaultValue>전체</option>
+            {users.map(user => (<option key={user.id} value={user.id}>{user.name}</option>))}
+          </Select>
+          <Heading
+            color={useColorModeValue('gray.700', 'white')}
+            fontSize={'2xl'}
+            fontFamily={'body'}
+            py={5}>
+            Message
+          </Heading>
+          <Input placeholder="Write your message" onChange={(e)=>setMessage(e.target.value)}/>
+        </Stack>
+        <Box py={5} >
+        <Button colorScheme="teal" size="sm" w="100%">
+            Send Message
+        </Button>
+        </Box>
+        <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
+          <Avatar
+            src={'https://avatars0.githubusercontent.com/u/1164541?v=4'}
+            alt={'Author'}
+          />
+          <Stack direction={'column'} spacing={0} fontSize={'sm'}>
+            <Text fontWeight={600}>I'm your ALOCADOS BOT!</Text>
+            <Text fontWeight={600}>Send message to users!</Text>
+          </Stack>
+        </Stack>
+        </Wrapper>
     )
 }
 
